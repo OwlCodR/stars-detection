7234a328b282441e1c7b56bcbe366ea1055d3379
OwlCodR <max.lyovkin@mail.ru>
2022-05-07 21:18:44 +0300

    :recycle: Refactored.

--- src/starsDetection.py
+++ src/starsDetection.py
@@ -5,7 +5,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
@@ -18,34 +17,62 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def classify(classifiers, X_train, X_test, y_train, y_test):
+    best_accuracy = 0
 
-table_name = 'stars3'
+    for classifier in classifiers:
+        classifier.fit(X_train, y_train)
+
+        prediction = classifier.predict(X_test)
+        
+        accuracy = accuracy_score(prediction, y_test)
+
+        print(accuracy)
+        print(classification_report(prediction, y_test))
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+        
+    print('Best accuracy: ', best_accuracy)
+
+
+def setStarsRatio(df, ratio):
+    """
+    Returns dataframe with stars ratio 
+    that says how many times the number of 
+    stars will exceed the number of other objects
+    """
+
+    notStarsCount = len(df['isStar'].loc[df['isStar'] == 0].index)
+    starsCount = len(df['isStar'].loc[df['isStar'] == 1].index)
+
+    print('notStarsCount:', notStarsCount)
+    print('starsCount:', starsCount)
+
+    df = df.sort_values('isStar')
+
+    print('Shape before setting ratio:', df.shape)
+    df = df.drop(df.index[notStarsCount * ratio:])
+    print('Shape after setting ratio:', df.shape)
 
 def main():
+    table_name = 'stars3'
+
     print('Connecting to database...')
     con = psycopg2.connect(user=user, password=password, host=host, port=port)
     cur = con.cursor()
     print('Done!')
 
-    sql = f'SELECT "FUVmag", "e_FUVmag", "NUVmag", "e_NUVmag", "Vmag", "e_Vmag", "Bmag", "e_Bmag", "otype" FROM {table_name}'
+    sql = f'''SELECT "FUVmag", "e_FUVmag", "NUVmag", "e_NUVmag", "Vmag", "e_Vmag", "Bmag", "e_Bmag", "otype" FROM {table_name}'''
 
     df = pd.read_sql_query(sql, con)
     print('Table:\n', df)
 
-    df['isStar'] = np.where((df['otype'] == 'Star') | (df['otype'].str.contains('\*') == True), 1, 0)
-
-    notStarsCount = len(df['isStar'].loc[df['isStar'] == 0].index)
-    starsCount = len(df['isStar'].loc[df['isStar'] == 1].index)
-
-    # df = df.sort_values('isStar')
-
-    print('notStarsCount:', notStarsCount)
-    print('starsCount:', starsCount)
-
-    # print('Shape before preprocessing:', df.shape)
-    # df = df.drop(df.index[notStarsCount * 2:])
-    # print('Shape after preprocessing:', df.shape)
+    df['isStar'] = np.where(
+        (df['otype'] == 'Star') | (df['otype'].str.contains('\*') == True), 1, 0)
     
+    # df = setStarsRatio(df, 1)
+
     df_features = df[df.columns[:-2]]
     print('Features:\n', df_features)
 
@@ -60,50 +87,22 @@ def main():
     # df['otype'].hist()
     # plt.show()
 
-    X_train, X_test, y_train, y_test = train_test_split(df_features, df_labels, test_size=0.1, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(df_features, df_labels, test_size=0.1)
 
     print('Train dataset: ', X_train.shape, y_train.shape)
     print('Test dataset: ', X_test.shape, y_test.shape)
 
-    svc = svm.SVC()
-    svc.fit(X_train, y_train)
-
-    svc_pred = svc.predict(X_test)
-
-    print('SVC:', accuracy_score(svc_pred, y_test))
-    print(classification_report(svc_pred, y_test))
-
-
-
-    log = LogisticRegression(max_iter=np.ceil(10**6 / y_train.shape[0]))
-    log.fit(X_train, y_train)
-
-    log_pred = log.predict(X_test)
-
-    print('LogisticRegression:', accuracy_score(log_pred, y_test))
-    print(classification_report(log_pred, y_test))
-
-
-
-
-    knn = KNeighborsClassifier(n_neighbors=5)
-    knn.fit(X_train, y_train)
-
-    knn_pred = knn.predict(X_test)
-
-    print('KNeighborsClassifier:', accuracy_score(knn_pred, y_test))
-    print(classification_report(knn_pred, y_test))
-
-
-
-    sgd = SGDClassifier(max_iter=np.ceil(10**6 / y_train.shape[0]))
-    sgd.fit(X_train, y_train)
-
-    sgd_pred = sgd.predict(X_test)
-
-    print('SGDClassifier:', accuracy_score(sgd_pred, y_test))
-    print(classification_report(sgd_pred, y_test))
-
+    max_iter = np.ceil(10**6 / (y_train.shape[0]))
+    
+    classify([
+            svm.SVC(),
+            GaussianNB(),
+            LogisticRegression(max_iter=max_iter),
+            KNeighborsClassifier(n_neighbors=5),
+            SGDClassifier(max_iter=max_iter)
+        ], 
+        X_train, X_test, y_train, y_test
+    )
 
 if __name__ == "__main__":
     main()
